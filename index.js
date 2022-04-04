3558a61c7df0217869180b9b5ba9590ab7f19b4c
KJeon10 <102023899+KJeon10@users.noreply.github.com>
2022-04-04 21:48:08 +0900

    update

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello NodeJS")
+import express from "express";
+
+const PORT = 4000;
+
+const app= express();
+
+app.get("/", () => console.log("get request to home!"));
+
+const handleListening = () =>
+console.log(`server listening on port ${PORT}`);
+
+app.listen(PORT, handleListening);

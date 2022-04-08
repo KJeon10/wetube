84051279da9c93cc944fd8731e70d7653575266c
KJeon10 <102023899+KJeon10@users.noreply.github.com>
2022-04-09 02:58:52 +0900

    update

--- src/init.js
+++ src/init.js
@@ -1,6 +1,7 @@
 import "./db.js";
 import "./models/video.js";
 import app from "./server.js";
+import "./models/User.js";
 
 const PORT = 4000;
 
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -2,11 +2,11 @@ import express from "express";
 import {
   watch,
   getEdit,
-  remove,
   getUpload,
   postUpload,
   search,
   postEdit,
+  deleteVideo,
 } from "../controllers/videoController.js";
 
 const videoRouter = express.Router();
@@ -14,7 +14,7 @@ const videoRouter = express.Router();
 videoRouter.get("/search", search);
 videoRouter.get("/:id([0-9a-f]{24})", watch);
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
-videoRouter.get("/:id(\\d+)/remove", remove);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
 
 export default videoRouter;
